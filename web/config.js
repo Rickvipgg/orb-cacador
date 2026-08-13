8838f3cdbac6af9805b5c0a49d0498b3ba9aa3bb
Henrique <henriqueleite3107@gmail.com>
2026-08-13 10:35:52 -0300

    Cria painel ORB Cacador

---
+++ web/config.js
@@ -0,0 +1,5 @@
+window.ORB_CONFIG = {
+  supabaseUrl: "https://aftgupbkqikffhqzkaxe.supabase.co",
+  supabasePublishableKey: "sb_publishable_i9sGp4DxEjrmM8_Mk4orFg_X_hrfX0i",
+  panelBucket: "orb-panel"
+};
